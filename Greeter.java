270ed5596fc9b5c537a76f7a9d56b58ed729c38a
Miguel Urena <urena_miguel82@outlook.com>
2023-08-06 11:00:37 -0700

    Hello World

---
+++ Greeter.java
@@ -0,0 +1,8 @@
+// This class represents a Greeter object that displays
+// A Hello message on the console
+public class Greeter {
+  
+  public void sayHello() {
+    System.out.println("Hello, World!");
+  }
+}
